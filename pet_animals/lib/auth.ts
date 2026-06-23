205c62bcde94c86ec6d04e3891c14b748a42b508
deepachaudharyaoct10 <deepachaudharyaug11@gmail.com>
2026-06-23 12:38:26 +0530

    Implemented teh auth fille

---
+++ pet_animals/lib/auth.ts
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import { NextRequest } from "next/server";
+
+const JWT_SECRET= process.env.JWT_SECRET as string;
+
+export type TokenPayload ={
+    userId: string,
+    role: ["user", "admin"];
+};
+
+
+export function signToken(payload:TokenPayload) {
+    return jwt.sign(payload, JWT_SECRET,{expiresIn: "7d"}) as string;
+}
+
+export function verifyToken(request:NextRequest) {
+    const authHeader= request.headers.get("authorization");
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        throw new Error(
+            "No token provided"
+        );
+    }
+
+    const token = authHeader.split(" ")[1];
+    return jwt.verify(token , JWT_SECRET) as TokenPayload;
+}
